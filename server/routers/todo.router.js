6be0b9a5c9a755351f4456f3f0348a1499f1b14d
Harryson Pointdujour <Harryson Pointdujour>
2016-11-08 19:03:16 -0500

    implemented the get one route for the todos api

--- server/routers/todo.router.js
+++ server/routers/todo.router.js
@@ -18,7 +18,18 @@ router.get('/todos', function(req, res){
     });
   });
 });
-router.get('/todos/:id', function(req, res){});
+router.get('/todos/:id', function(req, res){
+  Todo.find({_id:req.params.id}, function(err, foundTodo){
+    if(err){
+      res.status(500).json({
+        err:err
+      });
+    }
+    res.status(200).json({
+      todo: foundTodo
+    });
+  });
+});
 router.post('/todos', function(req, res){
   var todo = new Todo(req.body);
   todo.save(function(err){
