b46ebb6994c8553c2fba7de5cd0b28809bd1f95b
Harryson Pointdujour <Harryson Pointdujour>
2016-11-10 20:50:52 -0500

    implemented create method in TodoServer

--- public/js/services/todo.service.js
+++ public/js/services/todo.service.js
@@ -26,7 +26,15 @@
     function getAllTodos(){
       return todos;
     }
-    function createOneTodo(todo){}
+    function createOneTodo(todo){
+      $http.post('/todos', todo)
+           .then(function(response){
+             todos.push(todo);
+           })
+           .catch(function(err){
+             console.log(err);
+           });
+    }
     function updateOneTodo(index, todo){}
     function deleteOneTodo(index){}
   }
